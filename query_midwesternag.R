d7339719fc64842a932f0f710037c3cd44e27bea
Lesley Atwood <lesley.atwood@gmail.com>
2018-02-26 13:59:36 -0500

    CC diversity database included

--- query_midwesternag.R
+++ query_midwesternag.R
@@ -12,25 +12,14 @@ library(readxl)
 setwd("C:/Users/LWA/Desktop/SNAPP_Wood_2017/LiteratureReview")
 
 #import dataframes
-Ref <- read_excel("Synthesis_Database_Atwood.xlsx", sheet = "Reference")
+Ref <- read_excel("CCdiversity_Synthesis_Database_Atwood.xlsx", sheet = "Reference")
 
-ExpD_Loc <- read_excel("C:/Users/LWA/Desktop/SNAPP_Wood_2017/LiteratureReview/Synthesis_Database_Atwood.xlsx", sheet = "ExpD_Location")
+ExpD_Loc <- read_excel("CCdiversity_Synthesis_Database_Atwood.xlsx", sheet = "ExpD_Location")
 
-CashCrop <- read_excel("C:/Users/LWA/Desktop/SNAPP_Wood_2017/LiteratureReview/Synthesis_Database_Atwood.xlsx", sheet = "CashCrop")
+CashCrop <- read_excel("CCdiversity_Synthesis_Database_Atwood.xlsx", sheet = "CashCrop")
 
-CoverCrop <- read_excel("C:/Users/LWA/Desktop/SNAPP_Wood_2017/LiteratureReview/Synthesis_Database_Atwood.xlsx", sheet = "CoverCrop")
+CoverCrop <- read_excel("CCdiversity_Synthesis_Database_Atwood.xlsx", sheet = "CoverCrop")
 
-Results <- read_excel("C:/Users/LWA/Desktop/SNAPP_Wood_2017/LiteratureReview/Synthesis_Database_Atwood.xlsx", sheet = "Results")
+Results <- read_excel("CCdiversity_Synthesis_Database_Atwood.xlsx", sheet = "Results")
 
 
-#create empty database with no tables
-con = dbConnect(RSQLite::SQLite(), dbname = ":memory:")
-
-#inspect empty database
-dbListTables(con)
-
-#load data into the database
-dbWriteTable(con, "Ref", Ref)
-
-#inspect loaded dataframe
-dbListFields(con, "Ref")

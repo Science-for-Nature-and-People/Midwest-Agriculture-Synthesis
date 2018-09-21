cecc9866d7a37d0bfa14add3f8b63efb35ebd481
Lesley Atwood <lesley.atwood@gmail.com>
2018-09-21 12:07:09 -0400

    Introduction statement formated

--- summary_midwesternag.R
+++ summary_midwesternag.R
@@ -22,7 +22,7 @@ df <- filter(covercrops, !(Trt_id1>0)) #set dataframe to work with - only using 
 #df <- filter(mixtures, !(Trt_id1>0)) #set dataframe to work with - only using comparisions to control (0)
 df <- arrange(df, Paper_id)
 
-     
+
 ###Synthesis Output################################################################################################################
 #Run this code for each metric grouping (from query script)   
           
@@ -45,7 +45,31 @@ df <- arrange(df, Paper_id)
 ###Paper-level synthesis##################################################################################################           
 
         #Prepare df####
-                  ###Create City, State lists#####
+                  
+                ###Create conditional statement for experimental design and experimental arrangement####
+            
+                            Experiment_row <- df %>%
+                                            select(Exp_design, Exp_arrangement, key, Paper_id) %>%
+                                            mutate(
+                                            Exp_list_row = case_when(
+                                                  !is.na(Exp_arrangement)  ~ paste(Exp_arrangement, Exp_design),
+                                                  !is.na(Exp_design)  ~ paste(Exp_design)
+                                            )) 
+              unique(Experiment_row$Exp_list_row)
+                    
+                            #Generate list of treatments included in each experiment.
+                              Exp_list <- Experiment_row %>%
+                              group_by(Paper_id) %>%         
+                              summarise (Exp_list = paste(unique(Exp_list_row), collapse=", "))
+                    
+                              #Inspect Treatment list 
+                                   View(Exp_list)
+                              
+                               #Attach column with list of treatments to dataframe
+                                df <- left_join(df, Exp_list)
+                              
+
+                ###Create City, State lists#####
                     
                     #Make column that combines city, state for each row
                     df$city_state <- paste(df$City, df$State, sep=", ")
@@ -93,11 +117,12 @@ df <- arrange(df, Paper_id)
                         df$Year_end <- df$Year_start + df$Years_num            
                      
                       #Concatenate year start and year end
-                                df$Years_exp <- paste(df$Year_start, df$Year_end, sep = " - ")
+                                df$Years_exp <- paste(df$Year_start, df$Year_end, sep = "-")
                                            
                     years_list <- df %>%
                                       group_by(Paper_id) %>% 
                                       summarise (years_list = paste(unique(Years_exp), collapse=" and "))
+                    unique(years_list)
                     
                     #Attach list of years study is conducted to dataset
                     df <- left_join(df, years_list, by="Paper_id")
@@ -108,6 +133,31 @@ df <- arrange(df, Paper_id)
                     df <- df %>%
                     group_by(Paper_id) %>%
                     mutate(unique_locations = n_distinct(Loc_multi))
+                    
+                   
+                
+                Unique_locations_text <- df %>%
+                                      select(unique_locations, key, Paper_id) %>%
+                                      mutate(
+                                      Unique_locs_text_row = case_when(
+                                            unique_locations == 1  ~ paste(unique_locations, "site"),
+                                            unique_locations != 1  ~ paste(unique_locations, "sites")
+                                      ))  
+                        
+           
+                        
+                        #Generate list of treatments included in each experiment.
+                        Unique_locs_text <- Unique_locations_text %>%
+                        group_by(Paper_id) %>%         
+                        summarise (locs_text = paste(unique(Unique_locs_text_row), collapse=", "))
+              
+                        #Inspect Treatment list 
+                             View(Unique_locs_text)
+                        
+                         #Attach column with list of treatments to dataframe
+                          df <- left_join(df, Unique_locs_text)
+                          colnames(df)
+                          
                     
           #Count number of replications for each experiment####
                     reps_list <- df %>%
@@ -122,12 +172,33 @@ df <- arrange(df, Paper_id)
                               #need to remove NAs (possibly with if else statement + is.na)
                               #need to add *and* before last treatment
       
-                        ######Merge replications (different # of replications per site)
-                        ######Merge experimental years (different start and end dates per site)...maybe distinguish differences for each site??
+                ######Dealing with NAs for Exp_arrangement & Exp_design
+            Trtmt_list_row <- df %>%
+                                    select(Trtmt_main:Trtmt_splitC, key, Paper_id) %>%
+                                    mutate(
+                                    Trtmt_list_row = case_when(
+                                          !is.na(Trtmt_splitC)  ~ paste(Trtmt_main, ", ", Trtmt_splitA, ", ", Trtmt_splitB, ", and " , Trtmt_splitC, sep = ""),
+                                          !is.na(Trtmt_splitB)  ~ paste(Trtmt_main, ", ", Trtmt_splitA, ", and ", Trtmt_splitB, sep = ""),
+                                          !is.na(Trtmt_splitA)  ~ paste(Trtmt_main," and ", Trtmt_splitA, sep = ""),
+                                          !is.na(Trtmt_main)  ~ paste(Trtmt_main)
+                                    )) 
+            
+             #Generate list of treatments included in each experiment.
+                              Trtmt_list <- Trtmt_list_row %>%
+                              group_by(Paper_id) %>%         
+                              summarise (Trtmt_list = paste(unique(Trtmt_list_row), collapse=", "))
+                    
+                              #Inspect Treatment list 
+                                   View(Trtmt_list)
                               
+                               #Attach column with list of treatments to dataframe
+                                df <- left_join(df, Trtmt_list)
+       
+                              
+                    
      #######INTRODUCTION Statement #################                        
             df$intro <- df %>% 
-                        str_glue_data("{Paper_id} A {Exp_design}, {Exp_arrangement} study with {reps_list} replications was conducted at {unique_locations} sites from {Year_start} to {Year_start + Years_num} in {city_state_list} investigating the effects of {Trtmt_list} in a {Cash_species} system ({Authors_abbrev}, {PubYear}).")
+                        str_glue_data("{Paper_id} A {Exp_list} study with {reps_list} replications was conducted at {locs_text} from {years_list} in {city_state_list} investigating the effects of {Trtmt_list} in a {Cash_species} system ({Authors_abbrev}, {PubYear}).")
 unique(df$intro)  
 
 

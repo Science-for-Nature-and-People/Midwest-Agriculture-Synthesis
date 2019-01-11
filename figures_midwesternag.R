12a235356be7ffba40776147eeb5c695b4a80008
LesleyAtwood <lesley.atwood@gmail.com>
2019-01-11 12:36:05 -0500

    Single summary data table added to script. Filter by response variable grouping of interest

--- figures_midwesternag.R
+++ figures_midwesternag.R
@@ -17,20 +17,22 @@ setwd("C:/Users/LWA/Desktop/SNAPP_Wood_2017/LiteratureReview")
 
 #cover Crop Review
 #import data -> summary files
-covercrops <- read.csv("C:/Users/LWA/github/midwesternag_synthesis/CoverCrop_data.csv", header=TRUE, row.names = "X")
-    cc_yield_summary <- read.csv(file = "C:/Users/LWA/github/midwesternag_synthesis/CoverCrop_Yield_Summary.csv", header=TRUE, row.names = "X")
-    cc_soil_summary <- read.csv(file = "C:/Users/LWA/github/midwesternag_synthesis/CoverCrop_Soil_Summary.csv", header=TRUE, row.names = "X")
-    cc_pest_summary <- read.csv(file = "C:/Users/LWA/github/midwesternag_synthesis/CoverCrop_Pest_Summary.csv", header=TRUE, row.names = "X")
-    cc_water_summary <- read.csv(file = "C:/Users/LWA/github/midwesternag_synthesis/CoverCrop_Water_Summary.csv", header=TRUE, row.names = "X")
+CC_summary_all <- read.csv("C:/Users/LWA/Desktop/github/midwesternag_synthesis/www/data/CoverCrop_Summary.csv", header=TRUE, row.names = "X")
+    
+
+    #cc_yield_summary <- read.csv(file = "C:/Users/LWA/Desktop/github/midwesternag_synthesis/CoverCrop_Yield_Summary.csv", header=TRUE, row.names = "X")
+    #cc_soil_summary <- read.csv(file = "C:/Users/LWA/Desktop/github/midwesternag_synthesis/CoverCrop_Soil_Summary.csv", header=TRUE, row.names = "X")
+    #cc_pest_summary <- read.csv(file = "C:/Users/LWA/Desktop/github/midwesternag_synthesis/CoverCrop_Pest_Summary.csv", header=TRUE, row.names = "X")
+    #cc_water_summary <- read.csv(file = "C:/Users/LWA/Desktop/github/midwesternag_synthesis/CoverCrop_Water_Summary.csv", header=TRUE, row.names = "X")
  
    
-    ###Figure for Cover Crop Review: Soils####
-    df <- 
-      #cc_soil_summary
-      #cc_water_summary 
-      #cc_yield_summary
-      cc_pest_summary[cc_pest_summary$mean_per_change < 1000,]
-    levels(df$Cover_crop_diversity) <- as.factor( df$Cover_crop_diversity)
+    ###Figures for Cover Crop Review####
+    df <- CC_summary_all[CC_summary_all$Group_RV == "Pest Regulation",]
+        df<- drop_na(df)
+      
+              # for Cover Crop, Pest Regulation with one very large outlier (>1000%)      
+            #df <- df[df$group_metric != "Seedcorn Maggot (#)",]
+    levels(df$Cover_crop_diversity2) <- as.factor( df$Cover_crop_diversity2)
     df$group_metric <- as.factor(df$group_metric)
     df <- df[with(df, order(Cover_crop_diversity, group_metric)),]
     
@@ -43,8 +45,8 @@ covercrops <- read.csv("C:/Users/LWA/github/midwesternag_synthesis/CoverCrop_dat
       geom_errorbar(aes(ymin = mean_per_change-sem_per_change, ymax = mean_per_change +sem_per_change, width=.1)) +
           geom_hline(yintercept=0, lty=2) +# add a dotted line at x=0 after flip
           coord_flip() + # flip coordinates (puts labels on y axis)
-          #xlab("group metric") + ylab("percent difference (%)") + # use a white background
-          labs(title ="Cover Crop Review", subtitle = "Pests & Natural Enemies", x = "", y = "percent difference (%)") + 
+          #xlab("group metric") + ylab("percent difference between control and treatment (%)") + # use a white background
+          labs(title ="Cover Crop Review", subtitle = "Pest Regulation", x = "", y = "percent difference between control and treatment (%)") + 
       #scale_fill_discrete(breaks=c("Monoculture","Mixture (2 Spp.)","Mixture (3+ Spp.)")) +        
       theme_bw() +
       #theme(legend.position = "right") +
@@ -57,6 +59,6 @@ covercrops <- read.csv("C:/Users/LWA/github/midwesternag_synthesis/CoverCrop_dat
     
     
     
-    ggsave("CC_pests.png")
+    ggsave("Cover Crop_pest.png")
     
     

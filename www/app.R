d3b65dd211db5a877d93f1da0b5adebaa7e0ccc2
Nathan Hwangbo <nathanhwangbo@ucsb.edu>
2019-01-28 16:13:57 -0800

    ordered the plots, changed when title updates, added text box below plot

--- www/app.R
+++ www/app.R
@@ -10,14 +10,15 @@ library("readxl")     # for importing dataframe
 library("ggplot2")    # for plotting data
 library(shinyjs)
 library(gdata)        #reorder legends
+library(shinydashboard)
 
 setwd(".")
-datapath <- "/Users/LWA/Desktop/github/midwesternag_synthesis/" 
-#datapath <- "/Users/nathan/Desktop/Midwest-Agriculture-Synthesis/www/data"
+#datapath <- "/Users/LWA/Desktop/github/midwesternag_synthesis/" 
+datapath <- "/Users/nathan/Desktop/Midwest-Agriculture-Synthesis/"
 
 #import data -> summary files
-covercrop <-  read.csv(file.path(datapath, "Cover Crop Review/CC_FULL_Summary2.csv"))
-pestmgmt <-  read.csv(file.path(datapath, "PestMgmt Review/PestMgmt_FULL_Summary2.csv"))
+covercrop <-  read.csv(file.path(datapath, "Cover Crop Review/CC_FULL_Summary2.csv"), stringsAsFactors = FALSE)
+pestmgmt <-  read.csv(file.path(datapath, "PestMgmt Review/PestMgmt_FULL_Summary2.csv"), stringsAsFactors = FALSE)
 
 summary_all <- full_join(covercrop, pestmgmt)
 
@@ -29,8 +30,13 @@ levels(summary_all$Legend_1)
 
 #reorder the data for the legend
 summary_all$Legend_1 <- reorder.factor(summary_all$Legend_1, new.order = c("Monoculture", "Mixture (2 Spp.)", "Mixture (3+ Spp.)", "Soil", "Foliage","Seed", "Seed & Foliage" ))  
-                                                                            
+#rearrange the data according to the new ordering defined above
 summary_all <- summary_all %>% arrange(Legend_1)
+
+#make a temporary new column that just combines the group_metric and the main group. This new column has all rows then.
+#then we reorder this column, so that it will be organized by both facet(main group) and by group_metric
+summary_all$group_metric_facet <- with(summary_all, paste(group_metric, main_group, sep = "_"))
+summary_all$group_metric_facet <- reorder.factor(summary_all$group_metric_facet, new.order = sort(unique(summary_all$group_metric_facet), decreasing = TRUE))
 
 ###start of management button test (safe to delete this chunk)###
   #all of our data has cover cropping in this column. I make half of the data (randomly chosen) a different entry to see if the button works.
@@ -47,7 +53,7 @@ sidebarLayout(
   sidebarPanel( 
         radioButtons(inputId = "MgmtPractice", label = "Management Practice", 
           choices = unique(summary_all$Review), #will be expanded as review dataframes are populated
-          selected = "Cover Cropping"),
+          selected = "Cover Crop"),
 
         radioButtons(inputId = "RV", label = "Infield Agro-Environmental Response",
           choices = unique(summary_all$Group_RV) %>% sort(),
@@ -58,9 +64,18 @@ sidebarLayout(
           
 
   mainPanel(
-        #textOutput(outputId ="description_of_selection"),
-            plotOutput(outputId = "forestplot")
+            plotOutput(outputId = "forestplot"),
+            br(),
+            fluidRow(
+              box(
+                textOutput(outputId = "text_description"),
+                title = "Plot Description",
+                width = 10 #change width depending on how big you want the textbox to be. can go from 1-12
+                           #to use other features of box (like color, collapsable, ect..) we need to change fluidPage to dashboardPage
+                
+              )
             )
+        )
 
   )    
 )
@@ -79,29 +94,47 @@ server <- function(input, output) {
           }) 
    
            #build figure based on selected data
-  
+        
         output$forestplot <- renderPlot({
             
-          ggplot(df(), aes(group_metric, mean_per_change1, 
+          ggplot(df(), aes(group_metric_facet, mean_per_change1, #remember that group_metric_facet is the column ordered by main_group and group_metric
                            ymin = mean_per_change1-sem_per_change1, 
                            ymax = mean_per_change1 +sem_per_change1)) +
+            scale_x_discrete("", breaks = summary_all$group_metric_facet, label = summary_all$group_metric) + #this line relabels the x from "group_metric_main_group" to just "group_metric"
             geom_pointrange() +
             geom_errorbar(aes(ymin = mean_per_change1-sem_per_change1, 
                               ymax = mean_per_change1 +sem_per_change1, 
                               width=.1)) +
             geom_hline(yintercept=0, lty=2) +# add a dotted line at x=0 after flip
             coord_flip() + # flip coordinates (puts labels on y axis)
-            labs(title =input$MgmtPractice, 
-                 subtitle = input$RV, 
+            labs(title =df()$Review[1], #since we are filtering summary_all to only have 1 value for review/group_rv, we can take any element as the label (they should all be the same)
+                 subtitle = df()$Group_RV[1], 
                  x = "",
                  y = "percent difference between control and treatment (%)") + 
             #scale_fill_discrete(breaks=c("Monoculture","Mixture (2 Spp.)","Mixture (3+ Spp.)")) +        
             theme_bw() +
             geom_point( aes(colour = Legend_1)) + #color labeling of fine level groupings
             facet_grid(main_group ~., scales = "free", space = "free") +
-            theme(strip.text.y = element_text(angle = 0))
-          
+            theme(strip.text.y = element_text(angle = 0)) 
         })
+        
+        output$text_description <- renderText({
+          if(df()$Review[1] == "Cover Crop"){
+            "this is the text we can show for Cover Crop options. We can add another if statement within this one if we want a separate description for each response. See commented code for an example"
+            # if(df()$Group_RV[1] == "Crop Production"){
+            #   "This is the text we show for cover crop & crop production"
+            # }
+            # else if(df()$Group_RV[1] =="Soil"){
+            #   "This is the text we show for cover crop and soil"
+            # }
+            # else if ... ect ect
+          }
+          else{
+            "this is the text we can show for Early Season Pest Management. The width of these text boxes can be adjusted in the ui, in the 'box' function"
+          }
+          
+          })
+        
         observe({
           click("update")  #this chunk will click the update button at the very start, so that our app starts with a plot.
                            # if you want to change the default plot, then change the default "selected" values above

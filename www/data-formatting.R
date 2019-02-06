26cef56d3feeeae000eb2be938b8f8cce6b97d25
Stephen Wood <stephenawood@gmail.com>
2019-02-05 19:33:54 -0500

    Re-organize code to have data formatting in separate script

---
+++ www/data-formatting.R
@@ -0,0 +1,45 @@
+library(dplyr)    # for sorting and summarizing data
+library(readxl)   # for importing dataframe
+library(DescTools)
+library(forcats)
+
+
+#### Load data ####
+setwd(".")
+datapath <- "~/Box Sync/Work/Code/Midwest-Agriculture-Synthesis/www/data"
+
+# import data -> summary files
+covercrop <- read.csv(file.path(datapath, "/CC_FULL_Summary.csv"), stringsAsFactors = FALSE)
+pestmgmt <- read.csv(file.path(datapath, "/PestMgmt_FULL_Summary2.csv"), stringsAsFactors = FALSE)
+
+
+#### Manipulate data ####
+summary_all <- full_join(covercrop, pestmgmt)
+
+# change columns to factors
+collist <- c("Review_id", "main_group", "group_metric", "Legend_1", "Legend_2", "Legend_3", "Group_RV", "Review")
+summary_all[collist] <- lapply(summary_all[collist], factor)
+summary_all$Legend_1 <- recode(summary_all$Legend_1,
+                               "Single species"="Monoculture", 
+                               "Two species"="Mixture (2 Spp.)",
+                               "Three or more species"="Mixture (3+ Spp.)"
+)
+
+# reorder the data for the legend
+summary_all$Legend_1 <- reorder.factor(summary_all$Legend_1, new.order = c("Monoculture", "Mixture (2 Spp.)", "Mixture (3+ Spp.)", "Soil", "Foliage", "Seed", "Seed & Foliage"))
+
+# rearrange the data according to the new ordering defined above
+summary_all <- summary_all %>% arrange(Legend_1)
+
+# make a temporary new column that just combines the group_metric and the main group.
+# This new column has all rows then.
+# then we reorder this column, so that it will be organized by both facet(main group)
+# and by group_metric
+summary_all$group_metric_facet <- with(summary_all, paste(group_metric, main_group, sep = "_"))
+# summary_all$group_metric_facet <- reorder.factor(summary_all$group_metric_facet, new.order = sort(unique(summary_all$group_metric_facet), decreasing = TRUE))
+
+summary_all %>%
+  group_by(Legend_1, Group_RV, Review) %>%
+  mutate(group_metric_facet = fct_reorder(group_metric_facet, mean_per_change1)) -> summary_all
+
+write.csv(summary_all,"~/Box Sync/Work/Code/Midwest-Agriculture-Synthesis/www/data/data-for-app.csv")

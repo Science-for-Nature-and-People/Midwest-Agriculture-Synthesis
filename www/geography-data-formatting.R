47e47e5ff1e387596904220ba8fbe90cd9d9e8e6
LesleyAtwood <lesley.atwood@gmail.com>
2019-02-21 14:14:45 -0500

    Review description added

--- www/geography-data-formatting.R
+++ www/geography-data-formatting.R
@@ -5,16 +5,20 @@
 library(tigris)
 library(sp)
 
-setwd("~/Box Sync/Work/Code/Midwest-Agriculture-Synthesis/www/data/mapping")
+#setwd("~/Box Sync/Work/Code/Midwest-Agriculture-Synthesis/www/data/mapping")
+setwd("C:/Users/LWA/Desktop/github/midwesternag_synthesis/www/data/mapping")
 
 counties <- counties(cb=TRUE)
 
 covercrop <- read_excel("CCdiversity_Synthesis_Database_Atwood.xlsx", 
                         sheet = "ExpD_Location")
+covercrop$Review <- paste("Cover Crops")
 nutrient <- read_excel("Nutrient_Synthesis_Database_Atwood.xlsx", 
                         sheet = "ExpD_Location")
+nutrient$Review <- paste("Fertility Management")
 pest <- read_excel("PestMgmt_Review_Atwood.xlsx", 
                        sheet = "ExpD_Location")
+pest$Review <- paste("Pest Management")
 
 all.data <- rbind(covercrop, nutrient, pest)
 
@@ -24,7 +28,8 @@ points <- SpatialPoints(all.data,
                           proj4string=CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
 
 countynames <- over(points, counties)
-final <- data.frame(all.data, countynames)[,c(1,2,6:11,15,19,33)]
+
+final <- data.frame(all.data, countynames)[,c(1,2,6:11,15,27,33)]
 
 write.csv(final,"site-data_with_counties.csv")
 

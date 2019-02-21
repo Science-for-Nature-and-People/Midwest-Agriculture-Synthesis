4c6fa459e550c74e8f10a8d7c1bfdd709ed8ebcd
Stephen Wood <stephenawood@gmail.com>
2019-02-21 14:28:04 -0500

    Integrated map into main app

--- www/appScripts/server.R
+++ www/appScripts/server.R
@@ -1,58 +1,83 @@
 #### SERVER INSTRUCTIONS ####
 server <- function(input, output, session) {
-  observeEvent(df0(), {
-    updateSelectInput(session, "RV", "Outcome",
-                      choices = unique(df0()$Group_RV),
-                      selected = unique(df0()$Group_RV)[1]
-    )
-  })
-  
-  observeEvent(df1(), {
-    updateSelectInput(session, "Legend_1", "Grouping",
-                      choices = unique(df1()$Legend_1),
-                      selected = unique(df1()$Legend_1) # add [1] to select option in list, remove (as is) for Default is select all options
-    )
-  })
-  
   # Reactive selection by management practice
   df0 <- eventReactive(input$MgmtPractice, {
-    
+
     # filter dataset to display selected review and response variables
     summary_all %>%
       filter(Review %in% input$MgmtPractice)
   })
-  
+
   # Next tier selection of reactive selection of outcome grouping
   df1 <- eventReactive(input$RV, {
     df0() %>%
       filter(Group_RV %in% input$RV)
   })
-  
+
   # Merge by cover crop type
   df2 <- eventReactive(input$update, {
-    
+
     # filter dataset to display selected review and response variables
     df1() %>%
       filter(Legend_1 %in% input$Legend_1) %>%
       group_by(Legend_1) %>%
       mutate(group_metric_facet = fct_reorder(group_metric_facet, mean_per_change1))
   })
-  
-  
-  # Build figure and map based on selected data
-  
-  output$Map <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      setView(lat = 40, lng = -91, zoom = 5)
+
+  # Filter by geography
+  df3 <- eventReactive(input$State, {
+    map.data %>%
+      filter(State %in% input$State)
   })
-  
+
+  observeEvent(df0(), {
+    updateSelectInput(session, "RV", "Outcome",
+      choices = unique(df0()$Group_RV),
+      selected = unique(df0()$Group_RV)[1]
+    )
+  })
+
+  observeEvent(df1(), {
+    updateSelectInput(session, "Legend_1", "Grouping",
+      choices = unique(df1()$Legend_1),
+      selected = unique(df1()$Legend_1) # add [1] to select option in list, remove (as is) for Default is select all options
+    )
+  })
+
+  observe({
+
+    # Function to define multi-line labels
+    labs <- lapply(seq(nrow(df3())), function(i) {
+      paste0(
+        "State: ", df3()[i, "State"], "<p>", "County: ", df3()[i, "NAME"],
+        "<p>", "Treatment: ", df3()[i, "Trtmt_main"], "<p>", "DOI: ", df3()[i, "DOI"]
+      )
+    })
+
+    # Print map with points
+    leafletProxy("map", data = df3()) %>%
+      clearShapes() %>%
+      addMarkers(
+        label = lapply(labs, HTML),
+        clusterOptions = markerClusterOptions()
+      )
+  })
+
+
+
+  # Build figure and map based on selected data
+
+  # Add base map and set scale
+  output$map <- renderLeaflet({
+    leaflet(map.data) %>%
+      addTiles() %>%
+      fitBounds(~ min(Longitude), ~ min(Latitude), ~ max(Longitude), ~ max(Latitude))
+  })
+
   output$forestplot <- renderPlot({
     ggplot(df2(), aes(group_metric_facet, mean_per_change1, # remember that group_metric_facet is the column ordered by main_group and group_metric
-                      ymin = mean_per_change1 - sem_per_change1,
-                      ymax = mean_per_change1 + sem_per_change1
+      ymin = mean_per_change1 - sem_per_change1,
+      ymax = mean_per_change1 + sem_per_change1
     )) +
       scale_x_discrete("", breaks = df2()$group_metric_facet, label = df2()$group_metric) + # this line relabels the x from "group_metric_main_group" to just "group_metric"
       geom_pointrange() +
@@ -65,7 +90,7 @@ server <- function(input, output, session) {
       coord_flip() + # flip coordinates (puts labels on y axis)
       labs(
         title = df2()$Review[1], # since we are filtering summary_all to only have 1 value for review/group_rv, we can take any element as the label (they should all be the same)
-      #  subtitle = df2()$Group_RV[1],
+        #  subtitle = df2()$Group_RV[1],
         x = "",
         y = "Percent difference between treatment and control (%)"
       ) +
@@ -73,10 +98,12 @@ server <- function(input, output, session) {
       theme_bw() +
       geom_point(aes(colour = Legend_1), size = 3) + # color labeling of fine level groupings
       facet_grid(main_group ~ ., scales = "free", space = "free") +
-      theme(legend.title = element_blank(), legend.position="top" ,
-            strip.text.y = element_text(angle = 0), text = element_text(size=14))
+      theme(
+        legend.title = element_blank(), legend.position = "top",
+        strip.text.y = element_text(angle = 0), text = element_text(size = 14)
+      )
   })
-  
+
   output$text_description <- renderText({
     if (df2()$Review[1] == "Cover Crop") {
       "Cover crops in all areas, on average, are positively related to soil properties, but insignificantly related to crop yield."
@@ -92,17 +119,17 @@ server <- function(input, output, session) {
       "Early season pest management is related to pests."
     }
   })
-  
-  #picks out the filtered data for download as a csv
+
+  # picks out the filtered data for download as a csv
   output$downloadData <- downloadHandler(
-    filename = "app_data.csv",  
-    content = function(file){
+    filename = "app_data.csv",
+    content = function(file) {
       write.csv(df2(), file, row.names = FALSE)
     }
   )
-  
+
   # Will click the update button at the start so the app starts with a plot.
   observe({
-    click("update") 
+    click("update")
   })
 }
--- www/appScripts/ui.R
+++ www/appScripts/ui.R
@@ -3,83 +3,72 @@
 # Managing Soil Carbon - SNAPP Working Group
 # -------------------------------------------
 
-#note: we can run this code outside of the ui/server scripts by using the command runApp('www/appScripts/') (assuming your working directory is up to Midwest-Agriculture-Synthesis)
+# note: we can run this code outside of the ui/server scripts by using the command runApp('www/appScripts/') (assuming your working directory is up to Midwest-Agriculture-Synthesis)
 
 #### Load libraries ####
 library(readr)
-library(tidyverse)
-library(shiny)    # for making Shiny app
-library(ggplot2)  # for plotting data
-library(leaflet)  # for mapping
-library(shinyjs)  # for interactive clicking
-library(gdata)    # reorder legends
+library(tidyverse) # For reading and manipulating data
+library(shiny) # for making Shiny app
+library(ggplot2) # for plotting data
+library(leaflet) # for mapping
+library(shinyjs) # for interactive clicking
+library(gdata) # reorder legends
 library(shinydashboard)
 
 #### Load data ####
-#setwd(".")
-# setwd("~/Box Sync/Work/Code/Midwest-Agriculture-Synthesis/www/data")
-# summary_all <- read_csv("data-for-app.csv")
+# setwd(".")
+setwd("~/Box Sync/Work/Code/Midwest-Agriculture-Synthesis/www/data")
+summary_all <- read_csv("data-for-app.csv")
+map.data <- read_csv("mapping/site-data_with_counties.csv")
 
 # #for nathan's system
 # datapath <- "/Users/nathan/Desktop/Midwest-Agriculture-Synthesis/www/data"
 # summary_all <- read_csv(file.path(datapath, "data-for-app.csv"))
 
 # #for Lesley's system
- # datapath <- "/Users/LWA/Desktop/github/midwesternag_synthesis/www/data"
- # summary_all <- read_csv(file.path(datapath, "data-for-app.csv"))
+# datapath <- "/Users/LWA/Desktop/github/midwesternag_synthesis/www/data"
+# summary_all <- read_csv(file.path(datapath, "data-for-app.csv"))
 
-
-#collist <- c("Review_id", "main_group", "group_metric", "Legend_1", "Legend_2", "Legend_3", "Group_RV", "Review")
-#summary_all[collist] <- lapply(summary_all[collist], factor)
 
 #### User Interface ####
 # user interface
 ui <- navbarPage(
   "Midwest Soil Health Evidence",
-  
+
   tabPanel(
     "Data",
-    
+
     # shinyjs required for the update button to initialize a plot
     useShinyjs(),
-    
-    tags$style(type='text/css', ".selectize-input { font-size: 11px; line-height: 11px;}"),
-    
+
+    tags$style(type = "text/css", ".selectize-input { font-size: 11px; line-height: 11px;}"),
+
     # Set up header columns
     fluidRow(
       column(
-        3,
+        4,
         align = "center",
         selectInput(
           inputId = "MgmtPractice", label = "Practice",
-          choices = unique(summary_all$Review) %>% sort(),# multiple = T,
+          choices = unique(summary_all$Review) %>% sort(), # multiple = T,
           selected = "Cover Crops"
         )
       ),
       column(
-        3,
+        4,
         align = "center",
         selectInput(
           inputId = "RV", label = "Outcome",
-          choices = unique(summary_all$Group_RV) %>% sort(), multiple = T, 
+          choices = unique(summary_all$Group_RV) %>% sort(), multiple = T,
           selected = "Soil"
         )
       ),
       column(
-        3,
+        4,
         align = "center",
         selectInput(
           inputId = "Legend_1", label = "Grouping",
-          choices = unique(summary_all$Legend_1) %>% sort(), multiple = T, 
-          selected = "Monoculture"
-        )
-      ),
-      column(
-        3,
-        align = "center",
-        selectInput(
-          inputId = "Geography", label = "State",
-          choices = unique(summary_all$Legend_1) %>% sort(), multiple = T, 
+          choices = unique(summary_all$Legend_1) %>% sort(), multiple = T,
           selected = "Monoculture"
         )
       )
@@ -91,23 +80,32 @@ ui <- navbarPage(
         actionButton(inputId = "update", label = "Update", style = "padding:4px; font-size:80%")
       )
     ),
-    
+
     hr(),
-    
+
     # Set up row with plots of map and forest plot
     fluidRow(
       column(
         4,
-        leafletOutput("Map")
+        leafletOutput("map"),
+
+        # Filter by state
+        absolutePanel(
+          top = 10, right = 10,
+          selectInput(
+            inputId = "State", label = "State", multiple = T, selected = unique(map.data$State),
+            choices = unique(map.data$State) %>% sort()
+          )
+        )
       ),
       column(
         8,
         plotOutput(outputId = "forestplot")
       )
     ),
-    
+
     hr(),
-    
+
     # Set up row for text entry
     fluidRow(
       column(
@@ -116,13 +114,13 @@ ui <- navbarPage(
         textOutput(outputId = "text_description")
       )
     ),
-    
+
     hr()
   ),
-  
+
   tabPanel("References"),
   tabPanel("Methods"),
-  downloadButton(outputId = "downloadData", label = "Download") #the download button goes to the bottom
+  downloadButton(outputId = "downloadData", label = "Download") # the download button goes to the bottom
 )
 
 # #### RUN THE APP ####

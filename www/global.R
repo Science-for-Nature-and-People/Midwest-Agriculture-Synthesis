d8b8b0195c073befbf618c01afae7273dd4374e4
Nathan Hwangbo <nathanhwangbo@ucsb.edu>
2019-07-16 15:52:49 -0700

    moved libraries to global

--- www/global.R
+++ www/global.R
@@ -1,8 +1,24 @@
+
+#### Load libraries ####
 library(readr)
-library(here)
+library(tidyverse) # For reading and manipulating data
+library(shiny) # for making Shiny app
+library(ggplot2) # for plotting data
+library(leaflet) # for mapping
+library(shinyjs) # for interactive clicking
+library(gdata) # reorder legends
+library(shinydashboard)
+library(here) # to deal with tehfact we are using a sub directory (www)
+library(RColorBrewer) #to color the plot
+library(grid) #to add annotations to the plot (using grobs)
+library(cowplot)
+
 # Main data sets
 #summary_all <- read_csv(here("www", "data", "data-for-app.csv"))
-summary_all <- read_csv(here("www", "data", "data-for-app.csv"), col_types = cols(paper_id_list1 = col_character(), paper_id_list2 = col_character(), paper_id_list3 = col_character()))
+summary_all <- read_csv(here("www", "data", "data-for-app.csv"), col_types = cols(paper_id_list1 = col_character(), 
+                                                                                  paper_id_list2 = col_character(), 
+                                                                                  paper_id_list3 = col_character()
+                                                                                  ))
 map.data <- read_csv(here("www","data", "mapping","site-data_with_counties.csv")) %>%
   mutate()
 references <- read_csv(here("www", "data", "references-for-app.csv"), col_types = cols(Paper_id = col_integer())) %>%

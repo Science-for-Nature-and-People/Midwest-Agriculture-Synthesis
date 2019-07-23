5c726b8cbf0b889bb12ddcf829142d33a031a4f3
Nathan Hwangbo <nathanhwangbo@ucsb.edu>
2019-07-23 16:57:41 -0700

    reformatting the dependencies in the app (df0,1,2 now change constantly, not just on update button, but the plot/refernece table change only on update button)

--- www/server.R
+++ www/server.R
@@ -12,41 +12,42 @@ server <- function(input, output, session) {
   
   # Reactive selection by management practice
     #we want this to update if the user clicks the update button, or if they click a new practice
-  df0 <- eventReactive(c(input$update, input$MgmtPractice), {
-
+  df0 <- eventReactive(input$MgmtPractice, {
+    cat(file = stderr(), 'df0 is updated \n ')
     # filter dataset to display selected review and response variables
     summary_all %>%
       filter(Review %in% input$MgmtPractice)
+    
     
   })
 
   # Next tier selection of reactive selection of outcome grouping
     # we want this to update if the user clicks the update button, if they click a new outcome, or if they click a new practice (since outcome depends on practice)
-  df1 <- eventReactive(c(input$update, input$RV, input$MgmtPractice), {
+  df1 <- eventReactive(c(df0(),input$RV), {
+    cat(file = stderr(), 'df1 is updated \n')
     df0() %>%
       filter(Group_RV %in% input$RV) 
-      
+    
+    
   })
 
   # Merge by cover crop type
-  df2 <- eventReactive(input$update, {
-    #check that an outcome and grouping are selected (chained together so that it's sequential)
-      #ie check outcome first. if there is an outcome, then check grouping
-    validate(
-      need(!is.null(input$RV), "Please select an Outcome!") %then%
-      need(!is.null(input$Legend_1), "Please select a Grouping!")
-    )
+  df2 <- eventReactive(c(df1(), input$Legend_1), {
+    
+    
+    cat(file = stderr(), 'df2 is updated \n \n')
     # filter dataset to display selected review and response variables
     df1() %>%
       filter(Legend_1 %in% input$Legend_1) %>%
       group_by(Legend_1) %>%
       mutate(group_metric_facet = fct_reorder(group_metric_facet, mean_per_change1)) %>%
       ungroup()
+    
   })
 
   
   # Filter by geography
-  df3 <- eventReactive(c(input$update, input$State),  {
+  df3 <- eventReactive(input$State,  {
     
     #pick out all the paper ids in the filtered dataset df1() is prefiltered for practice/outcome
       #the filter command in the line below filters the "grouping" seleciton
@@ -71,6 +72,7 @@ server <- function(input, output, session) {
   })
 
   #update practice on summary choice
+    #this changes MgmtPractice, triggering change in df0 -> df1 -> df2
   observeEvent(input$summaryPractice, {
     updateRadioButtons(session, "MgmtPractice", "Practice",
                              choices = unique(summary_all$Review) %>% sort(),
@@ -79,6 +81,7 @@ server <- function(input, output, session) {
   })
   
   #update Outcome on summary choice
+    #this changes RV, which triggers change in df1 -> df2
   observeEvent(input$summaryRV, {
     updateCheckboxGroupInput(session, "RV", "Outcome",
       choices = unique(df0()$Group_RV) %>% sort(),
@@ -88,6 +91,7 @@ server <- function(input, output, session) {
   })
   
   #update outcome on practices
+    #if the old RV isn't an option in the new practice, this changes RV, which triggers change in df1 -> df2
   observeEvent(#df0(),
     input$MgmtPractice,{
     new_outcomes <- unique(df0()$Group_RV) %>% sort
@@ -98,8 +102,10 @@ server <- function(input, output, session) {
     )
   })
 
+#update lagend based on the other two choices
+  #if  the old selected legend isn't an option in thew new practice/outcomes, then this changes legend_1, triggeirng change in df2
   observeEvent({
-    df1()
+    c(input$MgmtPractice, input$RV)
     #input$update
     }, {
     
@@ -135,23 +141,13 @@ server <- function(input, output, session) {
   # })
   
   
-  observeEvent(c(input$MgmtPractice, input$RV, input$Legend_1),{
+  #df2() is what shows up in the forest plot. We choose not to plot if there are less than 5 rows
+  observeEvent(df2(),{
     #count the number of observations in our final dataset.
-      #num_obs is equivalent to df2(),but made separately so it can be updated without clicking the update button
-      #this is super inefficient, but the easiest way I could think to add an error message BEFORE clicking the update button 
-      #since the datasets (df1, df2, ect.) only change after update is clicked.
-      #the reason this works is that the event is reactive on the inputs individually, NOT the update button
-  num_obs <- summary_all %>%
-       filter(Review %in% input$MgmtPractice) %>%
-       filter(Group_RV %in% input$RV) %>%
-       filter(Legend_1 %in% input$Legend_1) %>%
-       group_by(Legend_1) %>%
-       mutate(group_metric_facet = fct_reorder(group_metric_facet, mean_per_change1)) %>%
-       ungroup() %>%
-       nrow
   #if there are less than 5 rows, then we disable the update button and show an error message
     # otherwise we hide the error message
-    if(num_obs < 5){
+    #cat(file = stderr(), 'nrow df2 = ', nrow(df2()), '\n')
+    if(nrow(df2()) < 5){
       shinyjs::disable('update')
       shinyjs::show('no_data')
     }else{
@@ -191,24 +187,31 @@ server <- function(input, output, session) {
       fitBounds(~ min(Longitude), ~ min(Latitude), ~ max(Longitude), ~ max(Latitude))
   })
   
-
+  #this is a dataframe that WILL NOT change based on direct inputs, but only change when the update button is pressed
+  #this shows all the data in the current plot, and is NOT sensitive to changes in the radio/select inputs
+    #ie df2() changes whenever an input changes (eg input$RV). But the plot doesn't change when input$RV changes. 
+    #df_plot will reflect the data in the plot, not the current df2()
+  df_plot <- eventReactive(input$update,{
+    df2()
+  })
+  
+  #forestplot will change whenever makeplot changes (so whenever the update button is pressed)
   output$forestplot <- renderPlot({
-    #control_text should hold the control name
-    control_text <- control_lookup[which(control_lookup$review_name == df2()$Review[1]),2]
+    control_text <- control_lookup[which(control_lookup$review_name == df_plot()$Review[1]),2]
     
     #we use this dataframe to make sure that we only plot the control text on the bottom facet (not all the facets)
-    control_labels <- data.frame(main_group = factor(tail(sort(df2()$main_group),1),       #the facets are sorted alphabetically, so this pulls out the bottom one
-                                                     levels = df2()$main_group %>% unique), #we set the levels just to get rid of some warnings
+    control_labels <- data.frame(main_group = factor(tail(sort(df_plot()$main_group),1),       #the facets are sorted alphabetically, so this pulls out the bottom one
+                                                     levels = df_plot()$main_group %>% unique), #we set the levels just to get rid of some warnings
                                  group_metric_facet = 0,                                    #group_metric_facet is the x axis on the plot, so this will put the control text at x = 0
                                  mean_per_change1 = 0,                                      #mean_per_change1 is the y axis on the plot, so this plots the control text at y = 0
                                  sem_per_change1 = 0)                                       #this is just needed because the ggplot below uses this to calculate the error bars. number doesn't matter I think
-
     
-    ggplot(df2(), aes(group_metric_facet, mean_per_change1, # remember that group_metric_facet is the column ordered by main_group and group_metric
-      ymin = mean_per_change1 - sem_per_change1,
-      ymax = mean_per_change1 + sem_per_change1
+    
+    ggplot(df_plot(), aes(group_metric_facet, mean_per_change1, # remember that group_metric_facet is the column ordered by main_group and group_metric
+                      ymin = mean_per_change1 - sem_per_change1,
+                      ymax = mean_per_change1 + sem_per_change1
     )) +
-      scale_x_discrete("", breaks = df2()$group_metric_facet, label = df2()$group_metric) + # this line relabels the x from "group_metric_main_group" to just "group_metric"
+      scale_x_discrete("", breaks = df_plot()$group_metric_facet, label = df_plot()$group_metric) + # this line relabels the x from "group_metric_main_group" to just "group_metric"
       geom_pointrange() +
       geom_errorbar(aes(
         ymin = mean_per_change1 - sem_per_change1,
@@ -217,10 +220,10 @@ server <- function(input, output, session) {
       )) +
       geom_hline(yintercept = 0, lty = 2) + # add a dotted line at x=0 after flip
       coord_flip(clip = "off") + # flip coordinates (puts labels on y axis)
-                                 # clip = "off" allows for plot annotations outside the plot area (used for the control annotations below the x axis)
+      # clip = "off" allows for plot annotations outside the plot area (used for the control annotations below the x axis)
       labs(
-        title = df2()$Review[1], # since we are filtering summary_all to only have 1 value for review/group_rv, we can take any element as the label (they should all be the same)
-        #  subtitle = df2()$Group_RV[1],
+        title = df_plot()$Review[1], # since we are filtering summary_all to only have 1 value for review/group_rv, we can take any element as the label (they should all be the same)
+        #  subtitle = df_plot()$Group_RV[1],
         x = "",
         y = "Percent difference between treatment and control (%)"
       ) +
@@ -228,7 +231,7 @@ server <- function(input, output, session) {
       theme_bw() +
       geom_point(aes(colour = Legend_1), size = 3) + # color labeling of fine level groupings
       scale_color_brewer(palette = "Set2") +          # change colors using RColorBrewer package to be ok for red-green colorblind
-                                                      # see all options using RColorBrewer::display.brewer.all(colorblindFriendly = TRUE)
+      # see all options using RColorBrewer::display.brewer.all(colorblindFriendly = TRUE)
       facet_grid(main_group ~ ., scales = "free", space = "free") +
       theme(
         legend.title = element_blank(), legend.position = "top",
@@ -236,9 +239,10 @@ server <- function(input, output, session) {
         axis.title.x = element_text(margin = margin(t = 20)) #moves the x.axis down to make room for the control annotation
       ) + 
       geom_text(data = control_labels, label = control_text, vjust = 2.75)    #vjust puts the text underneath the x axis
-                                                                              #the control_lables dataframe specifies which facet to put the text, puts the text at the x axis (y=0), so vjust moves it down a little
+    #the control_lables dataframe specifies which facet to put the text, puts the text at the x axis (y=0), so vjust moves it down a little
     })
 
+  #the text description at the bottom only depends on managementPractice
   output$text_description <- renderText({
     if (input$MgmtPractice == "Cover Crops") {
       "Cover crops in all areas, on average, are positively related to soil properties, but insignificantly related to crop yield."
@@ -255,10 +259,10 @@ server <- function(input, output, session) {
     }
   })
   
-  #add reference table, filtered by the plot filters.
+  
+  
   output$reference_table <- renderTable({
-    
-    plot_filtered_paper_id <- df2()$paper_id_list1 %>% 
+    plot_filtered_paper_id <- df_plot()$paper_id_list1 %>% 
       lapply(function(x) strsplit(x, split = ",") %>%
                unlist %>%
                as.integer) %>% 
@@ -274,7 +278,7 @@ server <- function(input, output, session) {
   output$downloadData <- downloadHandler(
     filename = "filtered_app_data.csv",
     content = function(file) {
-      write.csv(df2(), file, row.names = FALSE)
+      write.csv(df_plot(), file, row.names = FALSE)
     }
   )
   
